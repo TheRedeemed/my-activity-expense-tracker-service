fc248cf7e590028200a7287a08262456b540beee
TheRedeemed <abdoulkaders77@gmail.com>
2020-12-25 16:06:44 -0500

    initial commit

---
+++ src/main/java/com/theredeemed/myactivityexpensetrackerservice/MyActivityExpenseTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.theredeemed.myactivityexpensetrackerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyActivityExpenseTrackerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyActivityExpenseTrackerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theredeemed/myactivityexpensetrackerservice/MyActivityExpenseTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theredeemed.myactivityexpensetrackerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyActivityExpenseTrackerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
