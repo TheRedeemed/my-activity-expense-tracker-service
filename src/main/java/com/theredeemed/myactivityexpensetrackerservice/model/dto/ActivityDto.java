d477658539296f59d043c45274290006cb0c118d
TheRedeemed <abdoulkaders77@gmail.com>
2020-12-27 14:49:59 -0500

    Activity controller, service and dto

---
+++ src/main/java/com/theredeemed/myactivityexpensetrackerservice/model/dto/ActivityDto.java
@@ -0,0 +1,18 @@
+package com.theredeemed.myactivityexpensetrackerservice.model.dto;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ActivityDto {
+    String title;
+    String description;
+    BigDecimal balance;
+    LocalDateTime updatedTimestamp;
+}
